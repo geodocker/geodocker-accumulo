944cf586b2f6fb6c19b62d45c4fed38e71d9dfec
Eugene Cheipesh <echeipesh@gmail.com>
2016-07-21 03:12:00 -0400

    Improve error handling and failure reporting

--- fs/sbin/entrypoint.sh
+++ fs/sbin/entrypoint.sh
@@ -1,42 +1,36 @@
 #! /usr/bin/env bash
-set -eo pipefail
+set -euo pipefail
+IFS=$'\n\t'
 source /sbin/hdfs-lib.sh
 source /sbin/accumulo-lib.sh
 
 # Run in all cases
-if [[ -n ${HADOOP_MASTER_ADDRESS} ]]; then
-  sed -i.bak "s/{HADOOP_MASTER_ADDRESS}/${HADOOP_MASTER_ADDRESS}/g" ${HADOOP_CONF_DIR}/core-site.xml
-  sed -i.bak "s/{HADOOP_MASTER_ADDRESS}/${HADOOP_MASTER_ADDRESS}/g" ${ACCUMULO_CONF_DIR}/accumulo-site.xml
-fi
-
-if [[ -n ${ACCUMULO_ZOOKEEPERS} ]]; then
-  sed -i.bak "s/{ACCUMULO_ZOOKEEPERS}/${ACCUMULO_ZOOKEEPERS}/g" ${ACCUMULO_CONF_DIR}/accumulo-site.xml
-fi
-
-if [[ -n ${ACCUMULO_SECRET} ]]; then
-  sed -i.bak "s/{ACCUMULO_SECRET}/${ACCUMULO_SECRET}/g" ${ACCUMULO_CONF_DIR}/accumulo-site.xml
-fi
-
-if [[ -n ${ACCUMULO_PASSWORD} ]]; then
-  sed -i.bak "s/{ACCUMULO_PASSWORD}/${ACCUMULO_PASSWORD}/g" ${ACCUMULO_CONF_DIR}/accumulo-site.xml
-fi
-
-export USER=${USER:-root}
-ensure_user $USER
-echo "Running as $USER"
+sed -i.bak "s/{HADOOP_MASTER_ADDRESS}/${HADOOP_MASTER_ADDRESS}/g" ${HADOOP_CONF_DIR}/core-site.xml
+sed -i.bak \
+  -e "s/{HADOOP_MASTER_ADDRESS}/${HADOOP_MASTER_ADDRESS}/g" \
+  -e "s/{ACCUMULO_ZOOKEEPERS}/${ACCUMULO_ZOOKEEPERS}/g" \
+  -e "s/{ACCUMULO_SECRET}/${ACCUMULO_SECRET}/g" \
+  -e "s/{ACCUMULO_PASSWORD}/${ACCUMULO_PASSWORD}/g" \
+  ${ACCUMULO_CONF_DIR}/accumulo-site.xml
 
 # The first argument determines this container's role in the accumulo cluster
-if [ -z "$1" ]; then
+ROLE=${1:-}
+if [ -z $ROLE ]; then
   echo "Select the role for this container with the docker cmd 'master', 'monitor', 'gc', 'tracer', or 'tserver'"
   exit 1
 else
-  case $1 in
+  case $ROLE in
     "master" | "tserver" | "monitor" | "gc" | "tracer")
-      wait_until_port_open ${ACCUMULO_ZOOKEEPERS} 2181
-      wait_until_port_open ${HADOOP_MASTER_ADDRESS} 8020
-      wait_until_hdfs_is_available
+      ATTEMPTS=7 # ~2 min before timeout failure
+      wait_until_port_open ${ACCUMULO_ZOOKEEPERS} 2181 || exit 1
+      wait_until_port_open ${HADOOP_MASTER_ADDRESS} 8020 || exit 1
+      wait_until_hdfs_is_available || exit 1
 
-      if [[ ($1 = "master") && ($2 = "--auto-init")]]; then
+      USER=${USER:-root}
+      ensure_user $USER
+      echo "Running as $USER"
+
+      if [[ ($ROLE = "master") && (${2:-} = "--auto-init")]]; then
         set +e
         accumulo info
         if [[ $? != 0 ]]; then
@@ -54,7 +48,7 @@ else
           exit 1
         fi
       fi
-      exec runuser -p -u $USER accumulo -- $1 ;;
+      exec runuser -p -u $USER accumulo -- $ROLE ;;
     *)
       exec "$@"
   esac
