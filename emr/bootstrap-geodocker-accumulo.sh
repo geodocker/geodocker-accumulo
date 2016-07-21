88d64045c9e10cb023f037c9604753dc8e9594e0
Eugene Cheipesh <echeipesh@gmail.com>
2016-07-21 03:13:12 -0400

    fix: environment variables

--- emr/bootstrap-geodocker-accumulo.sh
+++ emr/bootstrap-geodocker-accumulo.sh
@@ -64,16 +64,17 @@ fi
 YARN_RM=$(xmllint --xpath "//property[name='yarn.resourcemanager.hostname']/value/text()"  /etc/hadoop/conf/yarn-site.xml)
 DOCKER_ENV="-e USER=hadoop \
 -e HADOOP_MASTER_ADDRESS=$YARN_RM \
--e CCUMULO_ZOOKEEPERS=$YARN_RM \
+-e ACCUMULO_ZOOKEEPERS=$YARN_RM \
 -e ACCUMULO_SECRET=$ACCUMULO_SECRET \
 -e ACCUMULO_PASSWORD=$ACCUMULO_PASSWORD \
 -e INSTANCE_NAME=$INSTANCE_NAME"
 
+DOCKER_OPT="-d --net=host --restart=always"
 if is_master ; then
-    docker run -d --net=host --name=accumulo-master $DOCKER_ENV $IMAGE master
-    docker run -d --net=host --name=accumulo-monitor $DOCKER_ENV $IMAGE monitor
-    docker run -d --net=host --name=accumulo-tracer $DOCKER_ENV $IMAGE tracer
-    docker run -d --net=host --name=accumulo-gc $DOCKER_ENV $IMAGE gc
+    docker run $DOCKER_OPT --name=accumulo-master $DOCKER_ENV $IMAGE master --auto-init
+    docker run $DOCKER_OPT --name=accumulo-monitor $DOCKER_ENV $IMAGE monitor
+    docker run $DOCKER_OPT --name=accumulo-tracer $DOCKER_ENV $IMAGE tracer
+    docker run $DOCKER_OPT --name=accumulo-gc $DOCKER_ENV $IMAGE gc
 else # is worker
     docker run -d --net=host --name=accumulo-tserver $DOCKER_ENV $IMAGE tserver
 fi
