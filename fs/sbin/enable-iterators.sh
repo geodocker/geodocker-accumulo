cab6fc71f6aad27ea2d6f3f49f22ca912a71c036
Eugene Cheipesh <echeipesh@gmail.com>
2016-07-27 14:55:25 -0400

    fix: geomesa/geowave namespace classpath
    wildcards appear to be invalid

--- fs/sbin/enable-iterators.sh
+++ fs/sbin/enable-iterators.sh
@@ -4,9 +4,9 @@ ACCUMULO_USER="root"
 
 accumulo shell -u ${ACCUMULO_USER} -p ${ACCUMULO_PASSWORD} <<-EOF
   createnamespace geomesa
-  config -s general.vfs.context.classpath.geomesa=file:/opt/geomesa/accumulo/*.jar
+  config -s general.vfs.context.classpath.geomesa=file:///opt/geomesa/accumulo/geomesa-accumulo-distributed-runtime-${GEOMESA_VERSION}.jar
   config -ns geomesa -s table.classpath.context=geomesa
   createnamespace geowave
-  config -s general.vfs.context.classpath.geowave=file:/usr/local/geowave/accumulo/*.jar
+  config -s general.vfs.context.classpath.geowave=file:///usr/local/geowave/accumulo/geowave-accumulo.jar
   config -ns geowave -s table.classpath.context=geowave
 EOF
